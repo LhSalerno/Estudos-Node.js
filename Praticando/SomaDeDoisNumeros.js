29cd3527de4700fea08b691301094b203cd450e5
Luis Salerno <luis.h.salerno@gmail.com>
2022-09-28 21:02:47 -0300

    Praticando

---
+++ Praticando/SomaDeDoisNumeros.js
@@ -0,0 +1,10 @@
+/* Programa que soma
+dois numeros*/
+
+var valor1 = 100;
+var valor2 = 20;
+var total = 0;
+
+total = valor1 + valor2;
+console.log ("Programa que soma dois numeros");
+console.log (valor1 + " + " + valor2 + " = " + total);
---
+++ Praticando/media.js
@@ -0,0 +1,21 @@
+/* Programa que calcula media*/
+
+var a1 = 12;
+var a2 = 13;
+var a3 = 14;
+var a4 = 16;
+var a5 = 18;
+var a6 = 20;
+var total =0;
+var media =0;
+
+total = a1 + a2 + a3 + a4 + a5 +a6;
+media = total /6;
+console.log('Programa que faz a media de 6 numeros');
+console.log('1 valor: ' + a1);
+console.log('2 valor: ' + a2);
+console.log('3 valor: ' + a3);
+console.log('4 valor: ' + a4);
+console.log('5 valor: ' + a5);
+console.log('6 valor: ' + a6);
+console.log('media: ' + media);
